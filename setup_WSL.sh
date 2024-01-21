631699c776e4e708a7149d7148e13d0d388b245c
szabimeister <61993364+szabimeister@users.noreply.github.com>
2024-01-21 12:28:27 +0100

    Add files via upload

--- setup_WSL.sh
+++ setup_WSL.sh
@@ -1,19 +1,19 @@
 echo 'Updating all packages'
-sudo apt update && sudo apt upgrade
+sudo apt update && sudo apt upgrade -y
 
 echo 'Installing Firefox ESR through Mozilla Team PPA'
-sudo add-apt-repository ppa:mozillateam/ppa
+sudo add-apt-repository ppa:mozillateam/ppa -y
 sudo apt update
-sudo apt install -y firefox-esr
+sudo apt install firefox-esr -y
 
 echo 'Installing "build-essential" package'
 sudo apt update
-sudo apt-get install build-essential
+sudo apt-get install build-essential -y
 
 echo 'Installing "zip" and "unzip" package'
 sudo apt update
-sudo apt install zip && sudo apt install unzip
+sudo apt install zip -y && sudo apt install unzip -y
 
 echo 'Installing "java" package'
 sudo apt update
-sudo apt install default-jre
+sudo apt install default-jre -y
